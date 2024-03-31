b94f0f71284633b5bb0da3cdb0c722f10a191525
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-03-31 19:59:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://tourismhost.vercel.app"],
+    origin: ["*"],
     methods: "GET, POST, PUT, DELETE, HEAD",
     credentials: true,
 };
