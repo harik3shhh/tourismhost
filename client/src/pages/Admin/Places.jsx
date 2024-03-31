1d793f29577afa84748397fc3a0ad1314dc9e50a
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-03-31 20:45:20 +0530

    Update Places.jsx

--- client/src/pages/Admin/Places.jsx
+++ client/src/pages/Admin/Places.jsx
@@ -12,7 +12,7 @@ const Places = ({ sidebar }) => {
     //GET ALL PRODUCTS
     const getAllProducts = async () => {
         try {
-            const { data } = await axios.get('http://localhost:8000/api/v1/place/get-place');
+            const { data } = await axios.get('https://tourismhost-ubpc.vercel.app/api/v1/place/get-place');
             setProducts(data.products);
         } catch (error) {
             console.log(error);
@@ -40,7 +40,7 @@ const Places = ({ sidebar }) => {
                                 <div key={p._id} className="places-item">
                                     <Link to={`/dashboard/admin/place/${p.slug}`} className="places-link">
                                         <div className="places-card">
-                                            <img src={`http://localhost:8000/api/v1/place/place-photo/${p._id}`} className="places-card-image" alt={p.name} />
+                                            <img src={`https://tourismhost-ubpc.vercel.app/api/v1/place/place-photo/${p._id}`} className="places-card-image" alt={p.name} />
                                             <div className="places-card-body">
                                                 <h5 className="places-card-title">{p.name}</h5>
                                                 <p className="places-card-description">{p.description.substring(0, 30)}</p>
