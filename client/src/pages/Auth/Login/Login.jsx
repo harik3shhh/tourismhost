09be8365f56c2769caa5ffc2a91c467f7043abec
harikesh <138229790+harik3shhh@users.noreply.github.com>
2024-04-01 13:39:40 +0530

    Update Login.jsx

--- client/src/pages/Auth/Login/Login.jsx
+++ client/src/pages/Auth/Login/Login.jsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import Sidebar from '../../../components/Sidebar/Sidebar';
-import "./Login.css"
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../context/auth';
-import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import './Login.css';
 
-const Login = ({sidebar}) => {
+const Login = ({ sidebar }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [auth, setAuth] = useAuth()
+  const [auth, setAuth] = useAuth();
   const [showPassword, setShowPassword] = useState(false);
   const [input, setInput] = useState({
     email: '',
@@ -32,69 +32,94 @@ const Login = ({sidebar}) => {
     e.preventDefault();
 
     try {
-      const { data } = await axios.post('https://tourismhost-ubpc.vercel.app/api/auth/login', input);
+      const { data } = await axios.post('http://localhost:8000/api/auth/login', input);
       console.log(data);
       if (data?.success) {
-        setInput({email: "", password: ""});
+        setInput({ email: '', password: '' });
         console.log('Login Successful!');
-        toast.success("Login Successful");
+        toast.success('Login Successful');
 
         setAuth({
           ...auth,
           user: data.user,
           token: data.token,
-      });
+        });
 
-      localStorage.setItem("auth", JSON.stringify(data));
-      navigate(location.state||"/");
+        localStorage.setItem('auth', JSON.stringify(data));
+        navigate(location.state || '/');
       } else {
         toast.error(data?.message);
-        toast.error("Invalid Email of Password");
+        toast.error('Invalid Email of Password');
       }
     } catch (error) {
       console.log(error);
-      toast.error("Invalid email or password");
+      toast.error('Invalid email or password');
     }
   };
 
   // TYPING EFFECT
   const [text] = useTypewriter({
-    words: ['Beautiful Destinations', 'Travelling India.', 'Login to access more features'],
+    words: ['Beautiful Destinations.', 'Travelling India.'],
     loop: {},
   });
 
   return (
     <>
-     <Sidebar sidebar={sidebar}/>
+      <Sidebar sidebar={sidebar} />
       <div className="content">
-        <div className="text-content">
-          <div className="text-content-column">
-            <h1 >
-            <span style={{color: "blue"}}>{text}</span>
-            <Cursor cursorColor='rgb(233, 225, 238)'/>
-            </h1>
-            <h3>Travel anywhere. Travel anytime.</h3>
-            <h5>Ready for the Trip? Enter your email to Login or create an account.</h5>
+        <form onSubmit={handleSubmit} className="form">
+          <h2 style={{ textAlign: 'center' }}>Login</h2>
+          <br />
+          <hr />
+          <div className="text-content">
+            <div className="text-content-column">
+              <h2>
+                <span style={{ color: 'blue' }}>{text}</span>
+                <Cursor cursorColor="rgb(233, 225, 238)" />
+              </h2>
+              <h3>Travel anywhere. Travel anytime.</h3>
+              <h5>Ready for the Trip? Enter your email to Login or create an account.</h5>
+            </div>
           </div>
-        </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="form">
-            <input type="email" className='.inp' placeholder="Email Address" name="email" value={input.email} onChange={handleInput} required/>
+          <input
+            type="email"
+            className=".inp"
+            placeholder="Email Address"
+            name="email"
+            value={input.email}
+            onChange={handleInput}
+            required
+          />
 
-            {showPassword && (
-              <input className='.inp' type="password" placeholder="Password" name="password" value={input.password} onChange={handleInput} required/>
-            )}
+          {showPassword && (
+            <input
+              className=".inp"
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={input.password}
+              onChange={handleInput}
+              required
+            />
+          )}
 
-            {!showPassword && (
-              <button className='btn1' onClick={() => setShowPassword(true)}>Let's Go</button>
-            )}
+          {!showPassword && (
+            <button className="btn1" onClick={() => setShowPassword(true)}>
+              Let's Go
+            </button>
+          )}
 
-            <NavLink to="/forgot-password" ><p className='forgot'>forgot password?</p></NavLink>
-            <NavLink to="/register" ><p className='create'>create account</p></NavLink>
+          <NavLink to="/forgot-password">
+            <p className="forgot">forgot password?</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p className="create">create account</p>
+          </NavLink>
 
-            <button className='btn2' type="submit">Login</button>
-          </div>
+          <button className="btn2" type="submit">
+            Login
+          </button>
         </form>
       </div>
     </>
